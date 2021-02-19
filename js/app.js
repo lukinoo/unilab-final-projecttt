22364516f11338a54800cb6f96f8986a4878c5ff
luka <luka.bliadze.3@iliauni.edu.ge>
2021-02-19 15:56:07 -0800

    changes

--- js/app.js
+++ js/app.js
@@ -1,50 +1,48 @@
 
 events = () => {
-    document.addEventListener('load', function () {
-        animationFade = () => {
-            // intro texts
-            const introTexts = document.querySelectorAll('.main-intro-text');
-            // small delay on intro text
-            introTexts.forEach((introText,index) => {
-                introText.style.animation = `textpopup 0.6s ease ${index / 7 + 0.5}s forwards`;
+    animationFade = () => {
+        // intro texts
+        const introTexts = document.querySelectorAll('.main-intro-text');
+        // small delay on intro text
+        introTexts.forEach((introText,index) => {
+            introText.style.animation = `textpopup 0.6s ease ${index / 7 + 0.5}s forwards`;
+        });
+
+    };
+        navigationBar = () => {
+            const hamburger = document.querySelector('.hamburger');
+            const navBar = document.querySelector('.nav-links');
+
+            // navigation
+            hamburger.addEventListener('click', function () {
+                navBar.classList.toggle('nav-links-open');
+                hamburger.classList.toggle('hamburger-line');
             });
-    
         };
-            navigationBar = () => {
-                const hamburger = document.querySelector('.hamburger');
-                const navBar = document.querySelector('.nav-links');
-    
-                // navigation
-                hamburger.addEventListener('click', function () {
-                    navBar.classList.toggle('nav-links-open');
-                    hamburger.classList.toggle('hamburger-line');
+
+        parallaxEffect = () => {
+            const bigTitle = document.querySelector('.big-title');
+            const scrollButton = document.getElementById('scroll-top');
+
+            window.addEventListener('scroll', function() {
+                let scrollYO = pageYOffset;
+
+                // scrolltop button
+                if (scrollYO > 20) {
+                    scrollButton.classList.add('scrollbuttonFade');
+                } else {
+                    scrollButton.classList.remove('scrollbuttonFade');
+                }
+
+                scrollButton.addEventListener('click', () => {
+                    document.documentElement.scrollTop = 0;
                 });
-            };
-    
-            parallaxEffect = () => {
-                const bigTitle = document.querySelector('.big-title');
-                const scrollButton = document.getElementById('scroll-top');
-    
-                window.addEventListener('scroll', function() {
-                    let scrollYO = pageYOffset;
-    
-                    // scrolltop button
-                    if (scrollYO > 20) {
-                        scrollButton.classList.add('scrollbuttonFade');
-                    } else {
-                        scrollButton.classList.remove('scrollbuttonFade');
-                    }
-    
-                    scrollButton.addEventListener('click', () => {
-                        document.documentElement.scrollTop = 0;
-                    });
-    
-                    // big text parallax effect
-                    bigTitle.style.transform = `translate3d(0px, ${scrollYO * -2}px, 0px)`;
-                });
-            };
-    
-    });
+
+                // big text parallax effect
+                bigTitle.style.transform = `translate3d(0px, ${scrollYO * -2}px, 0px)`;
+            });
+        };
+
 
     // function call
     animationFade();
