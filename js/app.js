4519a9708be2b468444949d06b6a60c0fd54c897
luka <luka.bliadze.3@iliauni.edu.ge>
2021-02-23 18:16:19 -0800

    added scroll button

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ navigation = () => {
     const hamburger = document.querySelector('.hamburger');
     const navBar = document.querySelector('.nav-links');
     const introTexts = document.querySelectorAll('.main-intro-text');
+    const scrollButton = document.getElementById('scroll-top-button');
 
     hamburger.addEventListener('click', function () {
         hamburger.classList.toggle('hamburger-line');
@@ -14,6 +15,21 @@ navigation = () => {
             introText.style.animation = `textpop 0.4s ease ${index / 5 + 0.5}s forwards`;
         });
     };
+
+    window.addEventListener('scroll', function () {
+        let scrolledY = pageYOffset;
+
+        if (scrolledY > 30) {
+            scrollButton.classList.add('scroll-button-active');
+        } else {
+            scrollButton.classList.remove('scroll-button-active');
+        }
+
+        scrollButton.addEventListener('click', () => {
+            document.documentElement.scrollTop = 0;
+        });
+
+    });
 
     introFade();
 };
